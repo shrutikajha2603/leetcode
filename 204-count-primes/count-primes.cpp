d48dcd9eddab67b3675e26b7db4767f6ce1cfc33
Shrutika Jha <shrutikajha2603@gmail.com>
2023-12-08 21:29:01 +0530

    Time: 133 ms (97.14%) | Memory: 35.4 MB (12.21%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,31 +1,21 @@
-#include <vector>
-#include <cmath>
-
 class Solution {
 public:
     int countPrimes(int n) {
-        if (n <= 2) {
-            return 0;
-        }
-
-        std::vector<bool> isPrime(n, true);
-        isPrime[0] = isPrime[1] = false;
-
-        for (int i = 2; i <= sqrt(n); i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j < n; j += i) {
-                    isPrime[j] = false;
-                }
+            int count = 0;
+            bool* isPrime = new bool[n];
+            for(int i = 2; i<n; i++){
+                    isPrime[i] = true;
             }
-        }
-
-        int count = 0;
-        for (bool prime : isPrime) {
-            if (prime) {
-                count++;
+            for(int i =2; i*i<n; i++){
+                    if(!isPrime[i]) continue;
+                    for(int j = i*i; j<n; j+=i){
+                            isPrime[j]= false;
+                    }
             }
-        }
-
-        return count;
+            for(int i =2; i<n;i++){
+                    if(isPrime[i]) count++;
+            }
+            return count;
+        
     }
 };
