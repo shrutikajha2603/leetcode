c1bdbe7b90f54a79d601dea98af5fe2d7e5dad0f
Shrutika Jha <shrutikajha2603@gmail.com>
2023-12-11 00:06:23 +0530

    Time: 13 ms (24.79%) | Memory: 6.4 MB (46.93%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -2,9 +2,15 @@ class Solution {
 public:
     int fib(int n) {
         int fn;
-        if (n==0) fn=0 ;
-        if (n==1) fn=1;
-        if (n>1) fn=(fib(n-1)+fib(n-2));
+        if (n==0){
+            fn=0 ;
+        }
+        else if(n==1){
+            fn=1;
+        }
+        else if(n>1){
+            fn=(fib(n-1)+fib(n-2));
+        }
         return fn;
     }
 };
