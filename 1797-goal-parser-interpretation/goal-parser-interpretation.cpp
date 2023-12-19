ad8b378fcc0d6693163a3023e51f1f3ef391caa0
Shrutika Jha <shrutikajha2603@gmail.com>
2023-12-19 14:39:13 +0530

    Time: 3 ms (45.28%) | Memory: 6.5 MB (95.14%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.cpp
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.cpp
@@ -3,12 +3,9 @@ public:
     string interpret(string command) {
         string ans;
         for(int i=0;i<command.size();i++){
-            if(command[i]=='G')
-                ans.push_back('G');
+            if(command[i]=='G') ans.push_back('G');
             else if(command[i]=='('){
-                if(command[i+1]==')'){
-                    ans.push_back('o');
-                }
+                if(command[i+1]==')') ans.push_back('o');
                 else{
                     ans.push_back('a');
                     ans.push_back('l');
