cbcf4461ff7b811e620f02f84120675e9adc0d99
Shrutika Jha <shrutikajha2603@gmail.com>
2024-01-12 18:30:42 +0530

    Time: 3 ms (52.79%) | Memory: 7.3 MB (11.73%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    bool halvesAreAlike(std::string s) {
-        auto countVowels = [](const std::string& str) {
-            std::unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+    bool halvesAreAlike(string s) {
+        auto countVowels = [](const string& str) {
+            unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
             int count = 0;
             for (char ch : str) {
                 if (vowels.find(ch) != vowels.end()) {
@@ -13,8 +13,8 @@ public:
         };
 
         int mid = s.length() / 2;
-        std::string a = s.substr(0, mid);
-        std::string b = s.substr(mid);
+        string a = s.substr(0, mid);
+        string b = s.substr(mid);
 
         return countVowels(a) == countVowels(b);
     }
