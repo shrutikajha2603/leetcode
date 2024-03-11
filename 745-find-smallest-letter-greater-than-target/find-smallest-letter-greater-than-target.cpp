cb186a010550184414c903831ff5c184cb80c4d7
Shrutika Jha <shrutikajha2603@gmail.com>
2024-03-11 21:22:36 +0530

    Time: 15 ms (29.90%) | Memory: 19.4 MB (81.95%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        for (char letter : letters) {
-            if (letter > target) {
-                return letter;
+        int n = letters.size();
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        return letters[0];
+        return left < n ? letters[left] : letters[0];
     }
 };
