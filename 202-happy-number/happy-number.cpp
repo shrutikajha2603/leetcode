947c6ffa2b2f9e9f4d4a9d3a5899504f10933ab2
Shrutika Jha <shrutikajha2603@gmail.com>
2024-03-30 19:40:23 +0530

    Time: 4 ms (28.91%) | Memory: 7.2 MB (72.03%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int digitSquareSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+    
+    bool isHappy(int n) {
+        int slow = n, fast = n;
+        do {
+            slow = digitSquareSum(slow);
+            fast = digitSquareSum(digitSquareSum(fast));
+        } while (slow != fast);
+        
+        return slow == 1;
+    }
+};
